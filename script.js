b959d4990f4a313c4c8ce3077dad7ed8311f3286
MD-AHAD-KHAN-PATHAN <md.ahadkhanpathan.tct@gmail.com>
2023-07-18 20:15:14 +0600

    google sheet setup

--- script.js
+++ script.js
@@ -25,19 +25,14 @@
         sidemeu.style.right = "-200px";
     }
 
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbzupBLcZyiNcV4BXA0BX8FXYQ6M9uuycUnXgmAwfdQj6377eCfis3_eBVIOAAzs70Lh/exec';
-    const form = document.forms['submit-to-google-sheet'];
-    const msg = document.getElementById("msg");
-    form.addEventListener('submit', e => {
-      e.preventDefault()
-      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => {
-            msg.innerHTML = "Your Message Sent Successfully";
-            setTimeout(function(){
-                msg.innerHTML = "";
-            },3000)
-            form.reset();
-        })
-        .catch(error => console.error('Error!', error.message))
-    })
+    const scriptURL = 'https://script.google.com/macros/s/AKfycby_xtmTnoqCkorQJqKcXeL1wqqww0qVjRXYIMmKOPHOC5pU0_1Qkbs-HCYFQ3M0-VvFbg/exec'
+	const form = document.forms['contactform']
+		  
+	form.addEventListener('submit', e => {
+	e.preventDefault()
+	fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+		.then(response => alert("Thank you! submitted successfully." ))
+		.then(() => {  window.location.reload(); })
+		.catch(error => console.error('Error!', error.message))
+		})
 
